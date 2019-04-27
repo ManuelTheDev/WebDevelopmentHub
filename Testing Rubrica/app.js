750a517257017665ea3b4ddca350216d5e419442
ManuelTheDev <mjdelgado@pucp.pe>
2019-04-27 16:25:01 -0500

    agregado ValidarRubrica

--- Testing Rubrica/app.js
+++ Testing Rubrica/app.js
@@ -1,33 +1,51 @@
 var app = angular.module('app', [])
 
 app.controller('myController', function($scope){
+    //Los criterios estan agrupados por familias
+    //Cada familia contiene un arreglo de criterios
+
+    $scope.listaFamiliaCriterios = []
+
+
+
     $scope.listaCriterios = [
         {
             nombre: 'Realizó el diagrama de clases correctamente',
-            puntaje: '',
-            puntajeMaximo: '10'
+            puntajeAsignado: '2',
+            puntajeMaximo: '10',
+            comentario: 'Criterio 1'
         },
         {
             nombre: 'Realizó la arquitectura de la aplicación correctamente',
-            puntaje: '',
-            puntajeMaximo: '5'
+            puntajeAsignado: '1',
+            puntajeMaximo: '5',
+            comentario: 'Criterio 2'
         },
         {
             nombre: 'Cumple con los estándares de programación',
-            puntaje: '',
-            puntajeMaximo: '5'
+            puntajeAsignado: '5',
+            puntajeMaximo: '5',
+            comentario: 'Criterio 3'
         }
     ]
 
     $scope.agregarCriterio = function(){
         $scope.listaCriterios.push({
             nombre: $scope.newCriterio.nombre,
-            puntaje: '',
+            puntajeAsignado: '',
             puntajeMaximo: $scope.newCriterio.puntajeMaximo
         })
 
         $scope.newCriterio.nombre = ""
         $scope.newCriterio.puntajeMaximo = ""
+    }
+
+    $scope.registrarCalificacion = function(){
+        console.log('estoy registrando')
+    }
+
+    $scope.mostrarConfirmacion = function(){
+        window.alert('Se ha validado la corrección de la rúbrica correctamente')
     }
 
 
